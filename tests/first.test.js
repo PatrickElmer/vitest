0c617350bb2dd16d121a5fd56cba447e14a7acfb
Patrick Eʟᴍᴇʀ <62191274+PatrickElmer@users.noreply.github.com>
2025-09-21 21:48:14 +0200

    Update first.test.js

--- tests/first.test.js
+++ tests/first.test.js
@@ -1,5 +1,4 @@
 import { describe, it, expect } from 'vitest';
-import { generateRuntimeFunction } from '../../packages/csp/src/parser.js';
 
 describe('First', () => {
     describe('first', () => {
