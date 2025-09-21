90ba47ad62e35dbe9926d2acce3d585937f5d08c
Patrick Eʟᴍᴇʀ <62191274+PatrickElmer@users.noreply.github.com>
2025-09-21 21:47:26 +0200

    Create first.test.js

---
+++ tests/first.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import { generateRuntimeFunction } from '../../packages/csp/src/parser.js';
+
+describe('First', () => {
+    describe('first', () => {
+        it('should pass', () => {
+            expect(1).toBe(1);
+        });
+});
