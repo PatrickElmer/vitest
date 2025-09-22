647bae66490d87996fed70773ccb657db1703ef1
Patrick Eʟᴍᴇʀ <62191274+PatrickElmer@users.noreply.github.com>
2025-09-22 07:38:04 +0200

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,8 +1,12 @@
 import { describe, it, expect } from 'vitest'
-import returnOne from './index.js'
+import returnOne, { add } from './index.js'
 
 describe('First', () => {
     it('should pass', () => {
         expect(returnOne()).toBe(1)
     })
+    it('should add 1 and 2', () => {
+        expect(add(1, 2)).toBe(3)
+    })
+
 })
