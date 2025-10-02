027e4ff13fac71765b3c396ba9438987f9b6bb5b
Patrick Elmer <patrick@elmer.ws>
2025-10-02 16:28:22 +0200

    Move tests to directory

--- tests/vitest/index.test.js
+++ tests/vitest/index.test.js
@@ -1,9 +1,8 @@
 import { describe, it, expect } from 'vitest'
-import { add } from './index.js'
+import { add } from '../../index.js'
 
 describe('First', () => {
     it('should add 1 and 2', () => {
         expect(add(1, 2)).toBe(3)
     })
-
 })
